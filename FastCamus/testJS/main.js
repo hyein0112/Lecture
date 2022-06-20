0f3903042aec420c42c46d7f6ac5e3e0cb572f50
hyein0112 <bhi12134@gmail.com>
2022-06-20 23:29:25 +0900

    testJS

---
+++ FastCamus/testJS/main.js
@@ -0,0 +1,50 @@
+// 콘솔창 출력
+console.log('Hyein');
+
+// String
+let myName="HYEIN";
+let email = 'hyein_0112@naver.com';
+
+let hello = `hello ${myName} !!` 
+console.log(hello); // hello HYEIN !!
+
+// Number
+let number = 123;
+let opacity = 1.57;
+console.log(number, opacity); // 123 1.57
+
+// Boolean
+let isShow = false;
+let checked = true;
+
+console.log(isShow) // false
+console.log(checked) // true
+
+// Undefined
+let undef;
+let obj = { abc: 123} ;
+
+console.log(undef); // undefined
+console.log(obj.abc); // 123
+console.log(obj.xyz); // undefined
+
+// Null
+let empty = null;
+
+console.log(empty); // null
+
+// Object
+let user = {
+  name:'hyein',
+  email: 'hyein_0112@naver.com'
+};
+
+console.log(user.name); // hyein
+console.log(user.email); // hyein_0112@naver.com
+
+// Array
+let fruits = ['apple', 'banana', 'cherry'];
+
+console.log(fruits[0]); // apple
+console.log(fruits[1]); // banana
+console.log(fruits[2]); // cherry
