438c8ea7fefac4ade8acda8e2501718b5205f7d8
hyein0112 <bhi12134@gmail.com>
2022-07-10 18:16:20 +0900

    Context

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
 
 let painting = false;
 
@@ -6,27 +13,30 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if(!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  }else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
-  console.log(event)
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function onMouseLeave(event) {
-  stopPainting();
-}
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
