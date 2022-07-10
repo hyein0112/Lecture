44beb08689474574a7b8c44b30f98782bc8a40b5
hyein0112 <bhi12134@gmail.com>
2022-07-10 18:54:48 +0900

    주석

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -1,8 +1,8 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("jsCanvas"); // canvas
+const ctx = canvas.getContext("2d"); // context
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+ctx.strokeStyle = "#2c2c2c"; // 선 color
+ctx.lineWidth = 2.5; // 선 두께
 
 canvas.width = 700;
 canvas.height = 700;
@@ -20,23 +20,19 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  if(!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+  if(!painting) { 
+    ctx.beginPath(); // 경로 생성
+    ctx.moveTo(x, y); // 좌표를 x, y 위치로 이동 (선의 시작)
   }else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    ctx.lineTo(x, y); // 좌표를 x, y 위치로 이동 (선의 끝)
+    ctx.stroke(); // 선 출력
   }
-}
-
-function onMouseDown(event) {
-  painting = true;
 }
 
 
 if(canvas){
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousemove", onMouseMove); // 컨버스 위에서 커서가 움직일 때
+  canvas.addEventListener("mousedown", startPainting); // 컨버스를 클릭하고 있을 때
+  canvas.addEventListener("mouseup", stopPainting); // 켠벼스에서 마우스 버튼을 떼었을 때
+  canvas.addEventListener("mouseleave", stopPainting); // 커서가 컨버스를 벗어났을 때
 }
