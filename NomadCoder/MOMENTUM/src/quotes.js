190c3ed5754d5938fd169163b07841c44cf71945
hyein0112 <bhi12134@gmail.com>
2022-07-17 02:59:47 +0900

    random quote

--- NomadCoder/MOMENTUM/src/quotes.js
+++ NomadCoder/MOMENTUM/src/quotes.js
@@ -41,3 +41,10 @@ const quotes = [
     author: "Sheda Savage",
   },
 ];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todayQuote.quote;
+author.innerText = todayQuote.author;
