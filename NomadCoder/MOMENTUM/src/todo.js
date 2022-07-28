8be8e0bf07325fd2e81d603ac11d2a3bd183db67
hyein0112 <bhi12134@gmail.com>
2022-07-29 02:51:00 +0900

    adding ToDos

--- NomadCoder/MOMENTUM/src/todo.js
+++ NomadCoder/MOMENTUM/src/todo.js
@@ -1,11 +1,21 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li"); // li 요소 생성
+  const span = document.createElement("span"); // span 요소 생성
+  
+  li.appendChild(span); // span을 li의 자식요소로 추가
+  span.innerText = newTodo;
+  toDoList.appendChild(li); // li를 toDoList의 자식요소로 추가
+}
 
 function handleToDoSubmit(e) {
   e.preventDefault();
   const newTodo = toDoInput.value; // input value를 newTodo 변수에 저장
   toDoInput.value = ""; // input vlaue 삭제
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
