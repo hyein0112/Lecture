88e158f23ac385e4318c6336aeeacb0d228abe0b
hyein0112 <bhi12134@gmail.com>
2022-09-08 21:09:00 +0900

    useEffect

--- NomadCoder/react-for-beginners/src/App.js
+++ NomadCoder/react-for-beginners/src/App.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue(prev => prev +1)
+  console.log("I run all the time")
+  useEffect(function() {
+    console.log("Call the API")
+  }, [])
   return (
     <div>
       <h1>{counter}</h1>
