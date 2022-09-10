33b173c79e1e9331c7f1e63d4bc7d2f8210d632f
hyein0112 <bhi12134@gmail.com>
2022-09-10 21:37:45 +0900

    Deps

--- NomadCoder/react-for-beginners/src/App.js
+++ NomadCoder/react-for-beginners/src/App.js
@@ -1,17 +1,34 @@
-import { useState, useEffect } from "react";
+import {
+  useState,
+  useEffect
+} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue(prev => prev +1)
-  console.log("I run all the time")
-  useEffect(function() {
-    console.log("Call the API")
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue(prev => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value)
+  useEffect(function () {
+    console.log("I run only once.")
   }, [])
-  return (
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.")
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter])
+  useEffect(() => {
+    console.log("I run when keyword & counter change")
+  }, [keyword, counter])
+  return ( 
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-
+      <input value = {keyword}
+      onChange = {onChange}
+      type = "text"
+      placeholder = "Search here..." 
+      />
+      <h1> {counter} </h1> 
+      <button onClick = {onClick}> Click me </button>
     </div>
   );
 }
