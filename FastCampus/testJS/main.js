84740900aa9bf5940a5d1f486e43c021040783ce
hyein0112 <bhi12134@gmail.com>
2022-09-15 21:16:14 +0900

    Promise

--- FastCampus/testJS/main.js
+++ FastCampus/testJS/main.js
@@ -100,10 +100,41 @@ DOM API
 
 (3) Getter, Setter
 */
-const boxEl = document.querySelector('.box');
+// const boxEl = document.querySelector('.box');
 
-//setter
-boxEl.textContent = 'HYEIN';
+// //setter
+// boxEl.textContent = 'HYEIN';
 
-//getter
-console.log(boxEl.textContent);
+// //getter
+// console.log(boxEl.textContent); 
+
+
+
+/*
+Promise
+*/
+function increaseAndPrint(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const value = n+1
+      if(value === 5) {
+        const error = new Error()
+        error.name = 'ValueIsFiveError'
+        reject(error)
+        return;
+      }
+      console.log(value)
+      resolve(value)
+    }, 1000)
+  })
+}
+
+increaseAndPrint(0)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .catch(e => {
+    console.error(e)
+  })
